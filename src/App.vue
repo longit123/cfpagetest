da76a7829d0bc31ab97f5cacb4376c48c6e6716d
goodname <15999523783@163.com>
2022-01-25 21:11:39 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <VideoPlayer/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import VideoPlayer from './components/VideoPlayer'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    VideoPlayer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
